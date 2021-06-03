8a29c72cb2528152e861693ce1ef6b469dbfcc8e
Md Moniruzzaman Sojol <bsmrstueee90@gmail.com>
2021-06-03 10:20:22 +0600

    start with recipe app

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>The Recipe App is Start</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem modi
+        aliquam quod repudiandae quia impedit assumenda quibusdam quidem
+        expedita velit?
+      </Text>
+      {/* <Button>Learn More</Button> */}
+      {/* <Image
+        style={styles.tinyLogo}
+        source={require('@expo/snack-static/react-native-logo.png')}
+      /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
